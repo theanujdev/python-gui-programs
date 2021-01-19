bc064cc99f02a6769f46324ef0cbf95c8327f9cb
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-01-19 18:51:17 +0530

    added basic config, title, min-size

---
+++ hash-checksum-utility/hash-utility.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title('Hash Authentication Utility')
+root.geometry("1470x650")
+root.minsize(1070, 611)
+root.configure(bg="white")
+
+root.mainloop()
