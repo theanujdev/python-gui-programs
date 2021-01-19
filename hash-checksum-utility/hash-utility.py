27f40e2618ba763ffdbca9086b87429257a2b58f
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-01-19 18:53:52 +0530

    Added heading & statusbar

--- hash-checksum-utility/hash-utility.py
+++ hash-checksum-utility/hash-utility.py
@@ -7,4 +7,12 @@ root.geometry("1470x650")
 root.minsize(1070, 611)
 root.configure(bg="white")
 
+#---------Heading and statusbar-----------#
+heading_label = tk.Label(root, text="Hash Authentication Utility",
+                         font="Helvetica 30 bold", bg="white", fg='purple3')
+heading_label.pack(pady=10, side=tk.TOP, fill=tk.X)
+statusbar = tk.Label(root, text="STATUS BAR",
+                     relief='sunken', anchor='w', padx=20, pady=5)
+statusbar.pack(fill=tk.X, side=tk.BOTTOM)
+
 root.mainloop()
